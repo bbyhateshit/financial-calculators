302dcb33d5139829ec037d9e46bdbe7ab29c30f1
bbyhateshit <j.galeta@gmail.com>
2026-04-10 12:28:56 -0400

    system.out.print statements

--- src/main/java/com/pluralsight/CalOne.java
+++ src/main/java/com/pluralsight/CalOne.java
@@ -7,7 +7,17 @@ public record CalOne() {
     public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 
+// USER INPUTS :
+        System.out.print("Enter the loan amount: ");
+        double principal = scanner.nextDouble();
 
+        System.out.print("Enter the annual interest rate: ");
+        double annualRatePercent = scanner.nextDouble();
+
+        System.out.print("Enter the loan term in years: ");
+        int years = scanner.nextInt();
+
+        scanner.close();
 
 
     }
