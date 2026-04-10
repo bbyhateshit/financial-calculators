979d135f6415c78c6ad3509f050a3786867e6c39
bbyhateshit <j.galeta@gmail.com>
2026-04-10 12:24:00 -0400

    imported scanner and thinking about what to do next..

--- src/main/java/com/pluralsight/CalOne.java
+++ src/main/java/com/pluralsight/CalOne.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
+
+import java.util.Scanner;
+
 public record CalOne() {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
 }
